4e7d4d57d80d014d3a911325d3c6d91c417174ee
Medatarunvenkat <tarunvm0909@gmail.com>
2025-07-11 20:49:48 +0530

    Backend done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/authRoutes.js";
+import bookRoutes from "./routes/bookRoutes.js";
 const app=express()
 const PORT=process.env.PORT;
 
 app.use(express.json());
+app.use(cors())
 app.use("/api/auth",authRoutes);
-
+app.use("/api/books",bookRoutes);
 
 app.listen(PORT,()=>{
     console.log(`server started at PORT ${PORT}`);
