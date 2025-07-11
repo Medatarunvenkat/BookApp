5c2b98f824c700a97089d1df3d5edca1ffbe813b
Medatarunvenkat <tarunvm0909@gmail.com>
2025-07-11 06:43:19 +0530

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import express from "express";
+import { connectDB } from "./lib/db.js";
+import authRoutes from "./routes/authRoutes.js";
+const app=express()
+const PORT=process.env.PORT;
+
+app.use("/api/auth",authRoutes);
+
+
+app.listen(PORT,()=>{
+    console.log(`server started at PORT ${PORT}`);
+    connectDB();
+});
